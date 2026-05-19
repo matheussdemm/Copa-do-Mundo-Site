3bf60893a4f27e665f617f759217585f68eb1ec8
matheussdem <matheussdem65@gmail.com>
2026-05-19 17:05:35 -0300

    Modificação do desing e organização do create e edit.

--- app/views/partidas/create.php
+++ app/views/partidas/create.php
@@ -1,46 +1,73 @@
-<?php include("../app/views/templates/header.php"); ?>
-
-<h2>Cadastrar Partida</h2>
-
 <form method="POST">
 
-    Data:
-    <input type="date" name="data" required>
+    <div class="form-row">
 
-    Seleção 1:
-    <input type="text" name="selecao_1" required>
+        <div class="form-group">
+            <label>Data</label>
+            <input type="date" name="data" required>
+        </div>
 
-    Seleção 2:
-    <input type="text" name="selecao_2" required>
+        <div class="form-group">
+            <label>Seleção 1</label>
+            <input type="text" name="selecao_1" required>
+        </div>
 
-    Gols 1:
-    <input type="number" name="gols_1" required>
+        <div class="form-group">
+            <label>Seleção 2</label>
+            <input type="text" name="selecao_2" required>
+        </div>
 
-    Gols 2:
-    <input type="number" name="gols_2" required>
+    </div>
 
-    Estádio:
-    <input type="text" name="estadio">
+    <div class="form-row">
 
-    Fase:
+        <div class="form-group">
+            <label>Gols 1</label>
+            <input type="number" name="gols_1" required>
+        </div>
 
-    <select name="fase">
+        <div class="form-group">
+            <label>Gols 2</label>
+            <input type="number" name="gols_2" required>
+        </div>
 
-        <option>Grupos</option>
-        <option>Oitavas</option>
-        <option>Quartas</option>
-        <option>Semifinal</option>
-        <option>Final</option>
+        <div class="form-group">
+            <label>Estádio</label>
+            <input type="text" name="estadio">
+        </div>
 
-    </select>
+    </div>
 
-    Ano:
-    <input type="number" name="ano" required>
+    <div class="form-row">
 
-    <button type="submit">
-        Cadastrar
-    </button>
+        <div class="form-group">
+            <label>Fase</label>
+
+            <select name="fase">
+
+                <option>Grupos</option>
+                <option>Oitavas</option>
+                <option>Quartas</option>
+                <option>Semifinal</option>
+                <option>Final</option>
+
+            </select>
+        </div>
+
+        <div class="form-group">
+            <label>Ano</label>
+            <input type="number" name="ano" required>
+        </div>
+
+        <div class="form-group form-button">
+            <button type="submit">
+                Cadastrar
+            </button>
+        </div>
+
+    </div>
 
 </form>
 
 <?php include("../app/views/templates/footer.php"); ?>
+<link rel="stylesheet" href="css/style.css">
--- app/views/partidas/edit.php
+++ app/views/partidas/edit.php
@@ -4,44 +4,138 @@
 
 <form method="POST">
 
-    Data:
-    <input type="date"
-           name="data"
-           value="<?= $row['data'] ?>">
+    <!-- PRIMEIRA LINHA -->
 
-    Seleção 1:
-    <input type="text"
-           name="selecao_1"
-           value="<?= $row['selecao_1'] ?>">
+    <div class="form-row">
 
-    Seleção 2:
-    <input type="text"
-           name="selecao_2"
-           value="<?= $row['selecao_2'] ?>">
+        <div class="form-group">
+            <label>Data</label>
 
-    Gols 1:
-    <input type="number"
-           name="gols_1"
-           value="<?= $row['gols_selecao_1'] ?>">
+            <input
+                type="date"
+                name="data"
+                value="<?= $row['data'] ?>"
+                required
+            >
+        </div>
 
-    Gols 2:
-    <input type="number"
-           name="gols_2"
-           value="<?= $row['gols_selecao_2'] ?>">
+        <div class="form-group">
+            <label>Seleção 1</label>
 
-    Estádio:
-    <input type="text"
-           name="estadio"
-           value="<?= $row['estadio'] ?>">
+            <input
+                type="text"
+                name="selecao_1"
+                value="<?= $row['selecao_1'] ?>"
+                required
+            >
+        </div>
 
-    Ano:
-    <input type="number"
-           name="ano"
-           value="<?= $row['ano_copa'] ?>">
+        <div class="form-group">
+            <label>Seleção 2</label>
 
-    <button type="submit">
-        Salvar
-    </button>
+            <input
+                type="text"
+                name="selecao_2"
+                value="<?= $row['selecao_2'] ?>"
+                required
+            >
+        </div>
+
+    </div>
+
+    <!-- SEGUNDA LINHA -->
+
+    <div class="form-row">
+
+        <div class="form-group">
+            <label>Gols 1</label>
+
+            <input
+                type="number"
+                name="gols_1"
+                value="<?= $row['gols_selecao_1'] ?>"
+                required
+            >
+        </div>
+
+        <div class="form-group">
+            <label>Gols 2</label>
+
+            <input
+                type="number"
+                name="gols_2"
+                value="<?= $row['gols_selecao_2'] ?>"
+                required
+            >
+        </div>
+
+        <div class="form-group">
+            <label>Estádio</label>
+
+            <input
+                type="text"
+                name="estadio"
+                value="<?= $row['estadio'] ?>"
+            >
+        </div>
+
+    </div>
+
+    <!-- TERCEIRA LINHA -->
+
+    <div class="form-row">
+
+        <div class="form-group">
+
+            <label>Fase</label>
+
+            <select name="fase">
+
+                <option <?= $row['fase']=="Grupos" ? "selected" : "" ?>>
+                    Grupos
+                </option>
+
+                <option <?= $row['fase']=="Oitavas" ? "selected" : "" ?>>
+                    Oitavas
+                </option>
+
+                <option <?= $row['fase']=="Quartas" ? "selected" : "" ?>>
+                    Quartas
+                </option>
+
+                <option <?= $row['fase']=="Semifinal" ? "selected" : "" ?>>
+                    Semifinal
+                </option>
+
+                <option <?= $row['fase']=="Final" ? "selected" : "" ?>>
+                    Final
+                </option>
+
+            </select>
+
+        </div>
+
+        <div class="form-group">
+
+            <label>Ano</label>
+
+            <input
+                type="number"
+                name="ano"
+                value="<?= $row['ano_copa'] ?>"
+                required
+            >
+        </div>
+
+        <div class="form-group form-button">
+
+            <button type="submit">
+                Salvar Alterações
+            </button>
+
+        </div>
+
+    </div>
 
 </form>
 
